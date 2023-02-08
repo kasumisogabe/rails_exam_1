ddf6d47acd7b056b1bbfdd089f5ae0ec8b0dc59f
kasumi sogabe <ksm17sgb@gmail.com>
2023-02-08 11:40:03 +0900

    Pictuerモデル、マイグレーションファイルの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_07_084534) do
+ActiveRecord::Schema.define(version: 2023_02_08_023637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.text "image"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 2023_02_07_084534) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "pictures", "users"
 end
