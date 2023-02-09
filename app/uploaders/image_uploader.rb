21388cda0e7fd8fc397419d672779219aece51c1
kasumi sogabe <ksm17sgb@gmail.com>
2023-02-09 11:45:10 +0900

    投稿画像サイズ変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # storage :fog
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  process resize_to_limit: [800, 800]
+  process resize_to_limit: [200, 200]
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
