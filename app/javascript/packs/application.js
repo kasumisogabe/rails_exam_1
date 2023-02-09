a5f844563aab4c3da42da91d563903c85a83b918
kasumi sogabe <ksm17sgb@gmail.com>
2023-02-09 10:57:56 +0900

    ヘッダーとフッター実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap';
 import '../stylesheets/application';
+import '../stylesheets/user';
+
 
 Rails.start()
 Turbolinks.start()
